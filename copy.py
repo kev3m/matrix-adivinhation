dc26459d1826fc2622687f9006953a80674b8b80
unknown <keven_jkc@hotmail.com>
2022-05-02 17:45:23 -0300

    modularizando

--- copy.py
+++ copy.py
@@ -1,33 +1,37 @@
 import random
 
-# boardNumbers = 1
-# dificulty = ['M', 60]
 
-# matrice = []
-# board = []
-# for i in range(1, dificulty[1] + 1):
-#     matrice.append(i)
-#     random.shuffle(matrice)
+def receberConfiguracoes(quantTabuleiros,dificuldade, encerramento, rodadas) :
+    if quantTabuleiros == 1:
+        gameConfigs['Tabuleiros'] = 1
+    elif quantTabuleiros == 2:
+        gameConfigs['Tabuleiros'] = 2
+    else:
+        return 'O jogo só pode ser jogado com 1 ou 2 tabuleiros! Digite uma quantidade válida.'
 
-# if boardNumbers == 1 and dificulty == 'F':
-#         board = [[matrice[0],matrice[1],matrice[2]],
-#         [matrice[3],matrice[4],matrice[5]],
-#         [matrice[6],matrice[7],matrice[8]]]
-# if boardNumbers == 1 and dificulty[0] == 'M':
-#         board = [[matrice[0],matrice[1],matrice[2]],
-#         [matrice[3],matrice[4],matrice[5]],
-#         [matrice[6],matrice[7],matrice[8]]]
+    if dificuldade == 'F':
+        gameConfigs['Dificuldade'] = 3
+    elif dificuldade == 'M':
+        gameConfigs['Dificuldade'] = 4
+    elif dificuldade == 'D':
+        gameConfigs['Dificuldade'] = 5
+    
+    if encerramento == 1:
+        gameConfigs['Encerrar'].append(encerramento)
+        gameConfigs['Encerrar'].append(rodadas)
+    elif encerramento == 2:
+        gameConfigs['Encerrar'] = encerramento
+    return quantTabuleiros, dificuldade, encerramento, rodadas
 
 
-# print(board)
-def createMatrice(boardNumbers, dificulty):
+def createMatrice(boardNumbers, difficulty):
     numList = []
 
-    if dificulty == 3:
+    if difficulty == 3:
         interval = 30
-    elif dificulty == 4:
+    elif difficulty == 4:
         interval = 60
-    elif dificulty == 5:
+    elif difficulty == 5:
         interval = 100
 
     for i in range(1, interval + 1):
@@ -38,85 +42,69 @@ def createMatrice(boardNumbers, dificulty):
     matrice = []
     secondMatrice = []
 
-    for i in range(dificulty):
-            numbers = random.sample(numList, dificulty)
+    for i in range(difficulty):
+            numbers = random.sample(numList, difficulty)
             for j in numbers:
                 numList.pop(numList.index(j))
             matrice.append(numbers)
 
     if boardNumbers == 2:
-        for i in range(dificulty):
-            numbers = random.sample(copyofNumList, dificulty)
+        for i in range(difficulty):
+            numbers = random.sample(copyofNumList, difficulty)
             for j in numbers:
                 copyofNumList.pop(copyofNumList.index(j))
             secondMatrice.append(numbers)
-    
     return matrice, secondMatrice
     
-    
-        
-tab = int(input('a> '))
-dif = int(input('b> '))
-board1, board2 = createMatrice(tab,dif)
-print(board1)
-print(board2)
+          
+# tab = int(input('a> '))
+# dif = int(input('b> '))
+# board, board2 = createMatrice(tab,dif)
+# print(board)
+# print(board2)
 
-# def criarMatriz(boardNumbers, dificulty):
-#     matrice = []
-#     #Shuffle problem
-#     oldMatriz = []
-#     for i in range(1,dificulty+1):
-#         matrice.append(i)
-#         random.shuffle(matrice)
-#     oldMatriz = matrice
-#     if boardNumbers == 1:
-#         board = [[matrice[0],matrice[1],matrice[2]],[matrice[3],matrice[4],matrice[5]],[matrice[6],matrice[7],matrice[8]]]
-#         if dificulty == 60:
-#             board[0].append(matrice[9]),board[1].append(matrice[10]), board[2].append(matrice[11]) 
-#             board.append([matrice[12], matrice[13], matrice[14], matrice[15]])   
-#         elif dificulty == 100:
-#             board[0].append(matrice[9]),board[0].append(matrice[10]), board[1].append(matrice[11]), board[1].append(matrice[12]), board[2].append(matrice[13]),board[2].append(matrice[14])
-#             board.append([matrice[15], matrice[16], matrice[17], matrice[18], matrice[19]]),board.append([matrice[20], matrice[21], matrice[22], matrice[23], matrice[24]]) 
-#         return board
-#     elif boardNumbers == 2:
-#         board = [[matrice[0],matrice[1],matrice[2]],[matrice[3],matrice[4],matrice[5]],[matrice[6],matrice[7],matrice[8]]]
-#         random.shuffle(matrice)
-#         secondBoard = [[matrice[0],matrice[1],matrice[2]],[matrice[3],matrice[4],matrice[5]],[matrice[6],matrice[7],matrice[8]]]
-#         #MÉTODO COPY
-#         if dificulty == 60:
-#             board[0].append(oldMatriz[9]),board[1].append(oldMatriz[10]), board[2].append(oldMatriz[11]) 
-#             board.append([oldMatriz[12], oldMatriz[13], oldMatriz[14], oldMatriz[15]])  
-#             secondBoard[0].append(matrice[9]),secondBoard[1].append(matrice[10]), secondBoard[2].append(matrice[11])
-#             secondBoard.append([matrice[12], matrice[13], matrice[14], matrice[15]])   
-#         elif dificulty == 100:
-#             board[0].append(oldMatriz[9]),board[0].append(oldMatriz[10]), board[1].append(oldMatriz[11]), board[1].append(oldMatriz[12]), board[2].append(oldMatriz[13]),board[2].append(oldMatriz[14])    
-#             board.append([oldMatriz[15], oldMatriz[16], oldMatriz[17], oldMatriz[18], oldMatriz[19]]), board.append([oldMatriz[20], oldMatriz[21], oldMatriz[22], oldMatriz[23], oldMatriz[24]])
-#             secondBoard[0].append(matrice[9]), secondBoard[0].append(matrice[10]), secondBoard[1].append(matrice[11]), secondBoard[1].append(matrice[12]), secondBoard[2].append(matrice[13]), secondBoard[2].append(matrice[14])
-#             secondBoard.append([matrice[15], matrice[16], matrice[17], matrice[18], matrice[19]]), secondBoard.append([matrice[20], matrice[21], matrice[22], matrice[23], matrice[24]])    
-#         return board, secondBoard
+def somarMatriz(boardNumbers, difficulty):
+    c1,c2,c3 = (board[0][0] + board[1][0] + board[2][0]), (board[0][1] + board[1][1] + board[2][1]), (board[0][2] + board[1][2] + board[2][2])
+    l1, l2, l3 = (sum(board[0])), (sum(board[1])), (sum(board[2]))
+    somasTab = {'Colunas': [c1,c2,c3], 'Linhas': [l1,l2,l3] }
+    if boardNumbers == 2:
+        c_1,c_2,c_3 = (board2[0][0] + board2[1][0] + board2[2][0]), (board2[0][1] + board2[1][1] + board2[2][1]), (board2[0][2] + board2[1][2] + board2[2][2])
+        l_1, l_2, l_3 = (sum(board2[0])), (sum(board2[1])), (sum(board2[2]))
+        somasTab = {'Colunas': [c1,c2,c3], 'Linhas': [l1,l2,l3] 
+            ,'ColunasT2': [c_1,c_2,c_3], 'LinhasT2': [l_1,l_2,l_3]} 
 
-
-
-# if tab == 1:
-#         c1,c2,c3 = (board[0][0] + board[1][0] + board[2][0]), (board[1][0] + board[1][1] + board[2][1]), (board[2][0] + board[2][1] + board[2][2])
-#         l1, l2, l3 = (sum(board[0])), (sum(board[1])), (sum(board[2]))
-#         somasTab = {'Colunas': [c1,c2,c3], 'Linhas': [l1,l2,l3] }
-#         if dif == 60:
-#             c1 += board[3][0]
-#             c2 += board[3][1]
-#             c3 += board[3][2]
-#             c4 = board[0][3] + board[1][3] + board[2][3] + board[3][3]
-#             l4 = sum(board[3])
-#             somasTab = {'Colunas': [c1,c2,c3,c4], 'Linhas': [l1,l2,l3,l4] }
-#         elif dif == 100:
-#             c1 += board[2][0]
-#             c2 += board[3][1]
-#             c3 += board[3][2]
-#             c4 = board[0][3] + board[1][3] + board[2][3] + board[3][3] + board[4][3]
-#             c5 = board[0][4] + board[1][4] + board[2][4] + board[3][5] + board[4][4]
-#             l4 = sum(board[3])
-#             l5 = sum(board[4])
-#             somasTab = {'Colunas': [c1,c2,c3,c4,c5], 'Linhas': [l1,l2,l3,l4,l5] }
-
-# print(somasTab)
+    if difficulty == 4:
+        c1 += board[3][0]
+        c2 += board[3][1]
+        c3 += board[3][2]
+        c4 = board[0][3] + board[1][3] + board[2][3] + board[3][3]
+        l4 = sum(board[3])
+        somasTab['Colunas'].append(c4), somasTab['Linhas'].append(l4)
+        if boardNumbers == 2:  
+            c_1 += board2[3][0]
+            c_2 += board2[3][1]
+            c_3 += board2[3][2]
+            c_4 = board2[0][3] + board2[1][3] + board2[2][3] + board2[3][3]
+            l_4 = sum(board2[3]) 
+            somasTab['ColunasT2'].append(c_4), somasTab['LinhasT2'].append(l_4)     
+    elif dif == 5:
+        c1 += board[3][0] + board[4][0]
+        c2 += board[3][1] + board[4][1]
+        c3 += board[3][2] + board[4][2]
+        c4 = board[0][3] + board[1][3] + board[2][3] + board[3][3] + board[4][3]
+        c5 = board[0][4] + board[1][4] + board[2][4] + board[3][4] + board[4][4] 
+        l4 = sum(board[3])
+        l5 = sum(board[4])
+        somasTab = {'Colunas': [c1,c2,c3,c4,c5], 'Linhas': [l1,l2,l3,l4,l5] }
+        if boardNumbers == 2:  
+            c_1 += board2[3][0]
+            c_2 += board2[3][1]
+            c_3 += board2[3][2]
+            c_4 = board2[0][3] + board2[1][3] + board2[2][3] + board2[3][3] + board2[4][3]
+            c_5 = board2[0][4] + board2[1][4] + board2[2][4] + board2[3][4] + board2[4][4]
+            l_4 = sum(board2[3]) 
+            l_5 = sum(board2[4])
+            somasTab = {'Colunas': [c1,c2,c3,c4,c5], 'Linhas': [l1,l2,l3,l4,l5] 
+            ,'ColunasT2': [c_1,c_2,c_3,c_4,c_5], 'LinhasT2': [l_1,l_2,l_3,l_4,l_5]}           
+    return somasTab
 
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import random
+import copy
 gameConfigs = {'Tabuleiros': 0, 'Dificuldade': 0, 'Encerrar': []} #Dif = (F,M OU D)
 gameStats = { 'Jogador 1': [['Nome', 0],['Jogada X']], 'Jogador 2': [['Nome', 0],['Jogada X']]}
 #Encerrar recebe uma lista contendo [X,Y] se a opção selecionadar for por rodadas, sendo X a opção e Y o número de rodadas
-
-#Recebendo as configurações do jogo
 def receberConfiguracoes(quantTabuleiros,dificuldade, encerramento, rodadas) :
     if quantTabuleiros == 1:
         gameConfigs['Tabuleiros'] = 1
@@ -13,11 +12,11 @@ def receberConfiguracoes(quantTabuleiros,dificuldade, encerramento, rodadas) :
         return 'O jogo só pode ser jogado com 1 ou 2 tabuleiros! Digite uma quantidade válida.'
 
     if dificuldade == 'F':
-        gameConfigs['Dificuldade'] = 30
+        gameConfigs['Dificuldade'] = 3
     elif dificuldade == 'M':
-        gameConfigs['Dificuldade'] = 60
+        gameConfigs['Dificuldade'] = 4
     elif dificuldade == 'D':
-        gameConfigs['Dificuldade'] = 100
+        gameConfigs['Dificuldade'] = 5
     
     if encerramento == 1:
         gameConfigs['Encerrar'].append(encerramento)
@@ -25,42 +24,6 @@ def receberConfiguracoes(quantTabuleiros,dificuldade, encerramento, rodadas) :
     elif encerramento == 2:
         gameConfigs['Encerrar'] = encerramento
     return quantTabuleiros, dificuldade, encerramento, rodadas
-
-
-#Criando a matriz do jogo 
-def criarMatriz(boardNumbers, dificulty):
-    matriz = []
-    #Shuffle problem
-    oldMatriz = []
-    for i in range(1,dificulty+1):
-        matriz.append(i)
-        random.shuffle(matriz)
-    oldMatriz = matriz
-    if boardNumbers == 1:
-        board = [[matriz[0],matriz[1],matriz[2]],[matriz[3],matriz[4],matriz[5]],[matriz[6],matriz[7],matriz[8]]]
-        if dificulty == 60:
-            board[0].append(matriz[9]),board[1].append(matriz[10]), board[2].append(matriz[11]) 
-            board.append([matriz[12], matriz[13], matriz[14], matriz[15]])   
-        elif dificulty == 100:
-            board[0].append(matriz[9]),board[0].append(matriz[10]), board[1].append(matriz[11]), board[1].append(matriz[12]), board[2].append(matriz[13]),board[2].append(matriz[14])
-            board.append([matriz[15], matriz[16], matriz[17], matriz[18], matriz[19]]),board.append([matriz[20], matriz[21], matriz[22], matriz[23], matriz[24]]) 
-        return board
-    elif boardNumbers == 2:
-        board = [[matriz[0],matriz[1],matriz[2]],[matriz[3],matriz[4],matriz[5]],[matriz[6],matriz[7],matriz[8]]]
-        random.shuffle(matriz)
-        secondBoard = [[matriz[0],matriz[1],matriz[2]],[matriz[3],matriz[4],matriz[5]],[matriz[6],matriz[7],matriz[8]]]
-        #MÉTODO COPY
-        if dificulty == 60:
-            board[0].append(oldMatriz[9]),board[1].append(oldMatriz[10]), board[2].append(oldMatriz[11]) 
-            board.append([oldMatriz[12], oldMatriz[13], oldMatriz[14], oldMatriz[15]])  
-            secondBoard[0].append(matriz[9]),secondBoard[1].append(matriz[10]), secondBoard[2].append(matriz[11])
-            secondBoard.append([matriz[12], matriz[13], matriz[14], matriz[15]])   
-        elif dificulty == 100:
-            board[0].append(oldMatriz[9]),board[0].append(oldMatriz[10]), board[1].append(oldMatriz[11]), board[1].append(oldMatriz[12]), board[2].append(oldMatriz[13]),board[2].append(oldMatriz[14])    
-            board.append([oldMatriz[15], oldMatriz[16], oldMatriz[17], oldMatriz[18], oldMatriz[19]]), board.append([oldMatriz[20], oldMatriz[21], oldMatriz[22], oldMatriz[23], oldMatriz[24]])
-            secondBoard[0].append(matriz[9]), secondBoard[0].append(matriz[10]), secondBoard[1].append(matriz[11]), secondBoard[1].append(matriz[12]), secondBoard[2].append(matriz[13]), secondBoard[2].append(matriz[14])
-            secondBoard.append([matriz[15], matriz[16], matriz[17], matriz[18], matriz[19]]), secondBoard.append([matriz[20], matriz[21], matriz[22], matriz[23], matriz[24]])    
-        return board, secondBoard
 
 #Iniciando o game
 menu = int(input('''
@@ -89,6 +52,7 @@ while menu != 3:
     elif menu == 1:
         quantTab = int(input('Digite a quantidade de tabuleiros a serem jogados: '))
         dificuldade = input('''Selecione a dificuldade:
+        
 === Selecione a dificuldade ===
 
     [F] - Fácil (3x3)
@@ -97,6 +61,7 @@ while menu != 3:
     
 ================================    
 ''').upper()[0]
+        
         finalizar = int(input('''Selecione o modo de encerramento da partida:
     [1] - Por número de rodadas
     [2] - Ao revelar completamente um dos tabuleiros
@@ -112,16 +77,17 @@ while menu != 3:
 ================================  
 Digite o nickname do jogador 1
 ================================  
-    ''')
+''')
     gameStats['Jogador 1'][0][1] = jogador1Nick
     jogador2Nick = input('''
 ================================  
 Digite o nickname do jogador 2
 ================================  
-    ''')
+''')
     gameStats['Jogador 2'][0][1] = jogador2Nick
     menu = 3
 
-
 print(gameConfigs)
-print(criarMatriz(gameConfigs['Tabuleiros'], gameConfigs['Dificuldade']))
+print(gameStats)
+
+
