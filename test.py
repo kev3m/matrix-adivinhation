2fd8febf24a508b115e8bd4354eefefd1f57e78d
unknown <keven_jkc@hotmail.com>
2022-05-08 23:23:48 -0300

    i wanna die

--- test.py
+++ test.py
@@ -17,9 +17,28 @@
 #         maiMen1 = maiMen2 = 'Ambos acertaram a soma'
 #         return 6, maiMen1, maiMen2
 
-mat = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
-mat2 = []
-fakemat = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
+# mat = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+# mat2 = []
+# fakemat = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
+
+# def roundWinner(p1P, p2P,maiMen1, maiMen2):
+#     if p1P < p2P:
+#         return 1, maiMen1
+#     elif p1P > p2P:
+#         return 2, maiMen2
+#      #mesma aproximação   
+#     elif p1P == p2P:
+#         maiMen1 = maiMen2 = 'Mesma aproximação'
+#         return 3, maiMen1, maiMen2
+#     elif p1P == 0:
+#         maiMen1 = 'P1 Acertou a soma'
+#         return 4, maiMen1
+#     elif p2P == 0:
+#         maiMen2 = 'P2 Acertou a soma'
+#         return 5, maiMen2
+#     elif p1P == 0 and p1P == 0:
+#         maiMen1 = maiMen2 = 'Ambos acertaram a soma'
+#         return 6, maiMen1, maiMen2
 
 
 def createColumns(mat1, mat2):
@@ -38,6 +57,39 @@ def createColumns(mat1, mat2):
 
     return colunas, colunas2
 
-def tableReveal(roundwin, realtable, faketable, column, column2, p1P, p2P):#p1p e p2p são da função atribuirValores
-    if roundwin == 1:
+mat = [[1,2,3], [4,5,6], [7,8,9]]
+mat2 = []
+coluna1, colunas2 = createColumns(mat, mat2)
+fakemat = [[0,0,0], [0,0,0], [0,0,0]]
+print(coluna1, colunas2)
+for i in coluna1:
+    print(i)
 
+play = 'c1'
+test = True
+
+
+def returnNum(winnerPlay,maiorOumenor,coluna):
+    if winnerPlay == 'c1':
+        if maiorOumenor == True:
+            swapNum = max(coluna[0])
+        elif maiorOumenor == False:
+            swapNum = min(coluna[0])
+    return swapNum
+
+def searchindex(matriz,num):
+    for i in matriz:
+        if num in i:
+            return matriz.index(i), i.index(num)
+
+def tableSwap(numero,fakemat,matrizInd, numInd):
+    fakemat[matrizInd][numInd] = numero
+    return fakemat
+
+nume = returnNum(play,test,coluna1)
+matind, numind = searchindex(mat,nume)
+
+fakematr = tableSwap(nume,fakemat,matind,numind)
+
+for i in fakematr:
+    print(i)
