646bec592636e8e6c1496e1f2a6e36aff4ae8482
unknown <keven_jkc@hotmail.com>
2022-05-17 00:08:44 -0300

    refatorando as saidas

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 #Funções responsáveis por gerar as matrizes que serão utilizadas na partida.
 from functions.matricesGenerator import createMatrice, createFakeMatrice, createColumnsMatrice
 #Funções responsáveis por manipular os valores contidos nas matrizes utilizadas.
@@ -32,49 +33,74 @@ def receiveConfigs(boardNumbers,difficulty, closure, roundsNum) :
         gameConfigs['Encerrar'] = closure
     return boardNumbers, difficulty, closure, roundsNum
 
-
+def clearTerminal():
+    if os.name == 'nt':
+        return os.system('cls')
+    elif os.name == 'posix':
+        return os.system('clear')
+        
 def statusReceiver(p1P,p1Sum, p2P, p2Sum,statsTab):
     statsTab['Jogador 1'][1][0].append(p1P)
     statsTab['Jogador 1'][1][1].append(p1Sum)
     statsTab['Jogador 2'][1][0].append(p2P)
     statsTab['Jogador 2'][1][1].append(p2Sum)
 
+
+        
+    
+
 #Iniciando o game
-menu = int(input('''
-=== Selecione uma opção ========    
+menu = 0
+while menu != 3:
+    menu = int(input('''
+========== Selecione uma opção =============    
 
     [1] - Iniciar 
     [2] - Instruções
     [3] - Sair
 
-================================    
-'''))
-
-while menu != 3:
-    if menu == 2:
+============================================      
+➪ '''))
+    while menu == 2:
         print('Aqui jás instruções')
         menu = int(input('''
-=== Selecione uma opção ========    
+========== Selecione uma opção =============    
 
     [1] - Iniciar 
     [2] - Instruções
     [3] - Sair
 
-================================    
-'''))
+============================================      
+➪ '''))
+    clearTerminal()
+        
     #Configurando o game
-    elif menu == 1:
+    if menu == 1:
         quantTab = int(input('''
+=========== Configurando o jogo ============         
 === Selecione a quantidade de tabuleiros ===
 
     [1] - Tabuleiro único
     [2] - 1 Tabuleiro para cada jogador
     
-============================================ 
-'''))
+============================================  
+➪ '''))
+        clearTerminal()
+        while quantTab != 1 and quantTab != 2:
+            print('Falha na matrix! Entrada inválida')
+            quantTab = int(input('''
+=========== Configurando o jogo ============         
+=== Selecione a quantidade de tabuleiros ===
+
+    [1] - Tabuleiro único
+    [2] - 1 Tabuleiro para cada jogador
+    
+============================================  
+➪ '''))
+        clearTerminal()
         if quantTab == 2:
             print('''
-=====================================
+============================================  
  ________________
 |                |
 |    Falha na    | 
@@ -83,58 +109,69 @@ while menu != 3:
  ∧＿∧  ||   Pedimos perdão!
 ( ´ω`) ||   Apenas um tabuleiro está   
 /     づ    disponível no momento   
-=====================================
+============================================  
             ''')
             quantTab == 1
-        difficulty = int(input('''Selecione a difficulty:
-        
-=== Selecione a difficulty ===
+        difficulty = int(input('''
+========= Selecione a dificuldade ==========
 
     [3] - Fácil (3x3)
     [4] - Médil (4x4)
     [5] - Difícil (5x5) 
     
-================================    
-'''))
-
+============================================     
+➪ '''))
+        clearTerminal()
         while difficulty != 3 and difficulty != 4 and difficulty != 5:
-            print('Falha na matrix! difficulty inválida')
-            difficulty = int(input('''Selecione a difficulty:
-        
-=== Selecione a difficulty ===
+            print('Falha na matrix! dificuldade inválida')
+            difficulty = int(input('''
+========= Selecione a dificuldade ==========
 
     [3] - Fácil (3x3)
     [4] - Médil (4x4)
     [5] - Difícil (5x5) 
     
-================================    
-'''))
+============================================ 
+➪ '''))
 
 
-        finalizar = int(input('''Selecione o modo de encerramento da partida:
+        finalizar = int(input('''
+==== Selecione o modo de encerramento ======      
+               da partida
+
     [1] - Por número de rodadas
     [2] - Ao revelar completamente um dos tabuleiros
-'''))
-        
+
+============================================
+➪ '''))
+        clearTerminal()
         while finalizar != 1 and finalizar != 2:
             print('Falha na matrix! Encerramento inválido')
-            finalizar = int(input('''Selecione o modo de encerramento da partida:
+            finalizar = int(input('''
+==== Selecione o modo de encerramento ======      
+               da partida
+
     [1] - Por número de rodadas
     [2] - Ao revelar completamente um dos tabuleiros
-'''))
+
+============================================
+➪ '''))
 
         if finalizar == 1:
-            numRodadas = int(input('''Digite o número de rodadas: 
-'''))       
+            numRodadas = int(input('''
+======= Digite o número de rodadas =========
+➪ '''))       
             while numRodadas % 2 == 0:
                 print('Falha na matrix! O número de rodadas deve ser impar para evitar impates.')
-                numRodadas = int(input('''Digite o número de rodadas:        
-'''))     
+                numRodadas = int(input('''
+
+======= Digite o número de rodadas =========
+➪ '''))    
         elif finalizar == 2:
             numRodadas = ''
     #Retornando as configurações para o dicionário
+    clearTerminal()
     plays = [['c1', 'c2', 'c3', 'c4', 'c5'], ['l1', 'l2', 'l3', 'l4', 'l5']] #Utlizada para servir de referência para a função que retorna o(s) número(s) a ser(em) exibido(s)
-    
     contadorRodadas = 0
     receiveConfigs(quantTab, difficulty,finalizar, numRodadas)
     board, board2 = createMatrice(quantTab,difficulty)
@@ -148,36 +185,45 @@ while menu != 3:
     sumtab = sumMatrice(quantTab, difficulty, board, board2)
     fakeMatrice = createFakeMatrice(difficulty)
     jogador1Nick = input('''
-================================  
-Digite o nickname do jogador 1
-================================  
-''')
+  
+====== Digite o nickname do jogador 1 ====== 
+➪ ''')
     gameStats['Jogador 1'][0][0] = jogador1Nick
     jogador2Nick = input('''
-================================  
-Digite o nickname do jogador 2
-================================  
-''')
+
+====== Digite o nickname do jogador 2 ====== 
+➪ ''')
+    print('''
+=====================================''')
     gameStats['Jogador 2'][0][0] = jogador2Nick
     menu = 3
+    clearTerminal()
     #Iniciando o jogo com um tabuleiro
     if gameConfigs['Tabuleiros'] == 1:
         print(f'''O jogo foi iniciado com um tabuleiro para dois jogadores
         Boa sorte {gameStats['Jogador 1'][0][0]} e {gameStats['Jogador 2'][0][0]}''')
         #if gameConfigs['Encerrar'] == 2:
         print(f'''
-======Tabela guia para escolha das opções======
--------------
-| x | x | x | > l1
-| x | x | x | > l2
-| x | x | x | > l3 
--------------
-  ^   ^   ^
-  c1  c2  c3
+================== Tabela guia para escolha das opções =========================
 
+        Fácil                   Médio                       Díficil
+    -------------         -----------------         ---------------------
+    | x | x | x | > l1    | x | x | x | x | > l1    | x | x | x | x | x | > l1
+    | x | x | x | > l2    | x | x | x | x | > l2    | x | x | x | x | x | > l2
+    | x | x | x | > l3    | x | x | x | x | > l3    | x | x | x | x | x | > l3
+    -------------         | x | x | x | x | > l4    | x | x | x | x | x | > l4
+     ^   ^   ^            -----------------         | x | x | x | x | x | > l5
+     c1  c2  c3             ^   ^   ^   ^           ---------------------
+                           c1  c2  c3  c4             ^   ^   ^   ^   ^
+                                                      c1  c2  c3  c4  c5    
+
+===============================================================================
 ''')    
         if finalizar == 1:
             while contadorRodadas < gameConfigs['Encerrar']:
+                print(f'''
+========================== Round {contadorRodadas + 1} ========================
+                ''')
                 p1Play = input(f'''{gameStats['Jogador 1'][0][0]} | Digite a linha ou coluna que deseja chutar o valor: ''')
                 if difficulty == '3':
                     while p1Play != 'c1' and p1Play != 'c2' and p1Play != 'c3' and p1Play != 'l1' and p1Play != 'l2' and p1Play != 'l3':
@@ -216,7 +262,7 @@ Digite o nickname do jogador 2
                 fakeMatrice = fakematr
                 
                 print(f'''
- ===========Status da rodada===========    
+ ======================== Status da rodada ===============================    
                 ''')
                 if roundWin == 1:
                     print('O jogador 1 foi o vencedor da rodada')
@@ -249,7 +295,7 @@ Digite o nickname do jogador 2
                 elif roundWin == 6:
                     print('Os dois jogadores acertaram a soma! Verdadeiros mestres da matriz.')
                 #########FAZER PONTUAÇÃO #########FAZER PONTUAÇÃO #########FAZER PONTUAÇÃO
-                
+                print('')
                 for i in fakematr:
                     print(i)
 
@@ -287,7 +333,7 @@ Escolheu a jogada {quadPlay} de soma {numSumPlay}
                 statusReceiver(p1Play, p1PlaySum, p2Play, p2PlaySum, gameStats)
                 
                 winnerplay, moreOrLess = returnWinnerPlay(p1Play, p2Play, roundWin, biggerORsmaller, biggerORsmaller2)
-                numToSwap = returnNumToSwap(roundWin,winnerplay,biggerORsmaller, biggerORsmaller2,column, plays, l1ines)
+                numToSwap = returnNumToSwap(roundWin,winnerplay,biggerORsmaller, biggerORsmaller2,column, plays, lines)
                 matind, numind = searchNumIndexInMainBoard(board,numToSwap)
                 fakematr = tableSwap(numToSwap,fakeMatrice,matind,numind)
                 fakeMatrice = fakematr
@@ -346,8 +392,7 @@ Escolheu a jogada {quadPlay} de soma {numSumPlay}
                     numSumPlay = gameStats['Jogador 2'][1][1][i]
                     print(f'''
     {i + 1}º Rodada | {gameStats['Jogador 2'][0][0]}
-    Escolheu a jogada {quadPlay} de soma {numSumPlay} 
-    ''')
+    Escolheu a jogada {quadPlay} de soma {numSumPlay}''')
 
         if gameStats['Jogador 1'][0][1] > gameStats['Jogador 2'][0][1]:
             print(f'''
